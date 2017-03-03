fa8ad006ab8cac4464df8059d304d325283a53c4
haigecao(曹海歌) <haigecao@tencent.com>
2017-03-03 19:06:11 +0800

    更新

--- 基础知识/examples/2016.11.03-node-dns/resolve4.js
+++
@@ -1,9 +0,0 @@
-var dns = require('dns');
-
-dns.resolve4('www.qq.com', function (err, address) {
-    if (err)
-        throw err;
-
-    // ["14.215.177.37","14.215.177.38"]
-    console.log(JSON.stringify(address));
-});
--- 基础知识/node-master/test/parallel/test-http-request-invalid-method-error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const common = require('../common');
-const assert = require('assert');
-const http = require('http');
-
-assert.throws(
-  () => { http.request({method: '\0'}); },
-  common.expectsError(undefined, TypeError, 'Method must be a valid HTTP token')
-);
